987d7bc3d2ffc4eaf3b038cf6bdadb93e83844c2
Gal Peretz <sgalprz@galp-mbp.lan>
2020-02-28 00:07:34 +0200

    .

---
+++ utils.py
@@ -0,0 +1,26 @@
+import urllib.request
+import os
+from tqdm import tqdm
+
+
+class DownloadProgressBar(tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+
+def download_url(url, target_folder, filename):
+    # check if data exists
+    print("Check if data exists on disk")
+    if not os.path.isdir(target_folder):
+      print("Creating target folder")
+      os.mkdir(target_folder)
+    files = os.listdir(target_folder)
+    if not files:
+        print("Cannot find files on disk")
+        print("Downloading files")
+        with DownloadProgressBar(unit='B', unit_scale=True,
+                                 miniters=1, desc=url.split('/')[-1]) as t:
+            urllib.request.urlretrieve(url, filename=target_folder + filename, reporthook=t.update_to)
+    print("Download completed!")
